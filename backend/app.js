44bf3fdc452c4eb2c53d355b864f76c832b814ce
Yash4900 <satra.y@somaiya.edu>
2020-09-26 11:03:30 +0530

    database connection added

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
 
+//datasbase connection
+const mongoose = require('./database/dbcon');
+
+//CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//this will allow our application to use json data
+app.use(express.json())
+
 app.listen(3000, () => console.log("Server running on port 3000"));
