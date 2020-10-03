2a272041883cefa8bdc135f1380c2e0d7e67b305
Yash4900 <satra.y@somaiya.edu>
2020-10-03 15:08:55 +0530

    product service created

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private productService: ProductService ) { }
 
   ngOnInit(): void {
   }
---
+++ frontend/src/app/services/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  readonly baseurl = 'http://localhost:3000';
+
+  constructor( private http: HttpClient ) { }
+
+  // get all products from the backend
+  getAllProducts(limitOfResults: Number = 10) {
+    return this.http.get(this.baseurl+'/products', {
+      params: {
+        limit: limitOfResults.toString()
+      }
+    });
+  }
+}
